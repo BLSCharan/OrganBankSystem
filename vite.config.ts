fd34875e6357963b50ae6ecaf7daed1c233911a5
blscharan <blscharan2004@gmail.com>
2025-04-25 11:11:48 +0530

    sec

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/OrganBank/',
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000', // Adjust this if your backend is running on a different port
-    },
   },
 });
