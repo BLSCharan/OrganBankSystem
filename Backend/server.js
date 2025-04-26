29c06d3dd7e8122d91efebee32a19b3094fcc6d5
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-04-26 21:50:40 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,9 @@ mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true
 }).then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT, () => {
-        console.log(`Server running on port ${process.env.PORT}`);
+    
+    // Use '0.0.0.0' to allow external connections
+    app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
+        console.log(`Server running on port ${process.env.PORT || 5000}`);
     });
 }).catch((err) => console.log('Mongo error:', err));
